db02b696a78e5b748497e7da06e2a17d9ff8bf9b
Mark Miller <Mark.Miller@fnb.co.za>
2023-02-01 07:42:32 +0200

    This is secnd dev Feature 2

--- KdiffP/Program.cs
+++ KdiffP/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World! This is Feature 2 Dev 2");
+Console.WriteLine("Hello, World! This is Feature 2 Dev 2");
+
+Console.WriteLine("Hello, World! This is Feature 2 Dev 2");
