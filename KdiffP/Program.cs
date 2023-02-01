a8b4c456fda51743495409143c506069074c2e96
Mark Miller <Mark.Miller@fnb.co.za>
2023-02-01 07:41:24 +0200

    Dev 1 commit on Feature 1

--- KdiffP/Program.cs
+++ KdiffP/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World! Dev1 Feature branch 1");
+Console.WriteLine("Hello, World! Dev1 Feature branch 1");
+
+Console.WriteLine("Hello, World! Dev1 ");
