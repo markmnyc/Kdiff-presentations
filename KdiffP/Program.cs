cc3bce8275aee229302e5579cebd2aeab8c820ba
Mark Miller <Mark.Miller@fnb.co.za>
2023-02-01 07:39:17 +0200

    main commiter comit

--- KdiffP/Program.cs
+++ KdiffP/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World! main branch line 4");
+Console.WriteLine("Hello, World! this is the main branch commit");
+
+Console.WriteLine("Hello, World! main branch line 7");
